64f001fd0e313a4f3f38290f461ab079a44de64f
ilhammajidzaiman <ilhammajidzaiman07@gmail.com>
2023-06-07 13:48:03 +0700

    feat: adding copy to clipboard button

--- resources/views/private/developer/page/show.blade.php
+++ resources/views/private/developer/page/show.blade.php
@@ -10,15 +10,37 @@
 
     <div class="row justify-content-center">
         <div class="col-md-6">
-            {{-- <div class="card">
-                <div class="card-content">
-                    <div class="card-body"> --}}
+            <input type="hidden" name="myInput" id="myInput" value="{{ route('/') . '/page/' . $page->slug }}" readonly>
+            <button class="text-capitalize rounded-pill btn btn-sm btn-outline-primary mb-4" onclick="copyToClipboard(this)"
+                data-bs-toggle="tooltip" data-bs-placement="top" title="Copy to Clipboard">
+                <i class="bi bi-clipboard"></i>
+                copy link
+            </button>
             <h3>{{ $page->title }}</h3>
             <x-field-date :create="$page->created_at" :update="$page->updated_at" class="text-capitalize" />
             <div>{!! $page->content !!}</div>
-            {{-- </div>
-                </div>
-            </div> --}}
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script>
+        function copyToClipboard(button) {
+            var copyText = document.getElementById("myInput");
+            copyText.select();
+            copyText.setSelectionRange(0, 99999);
+            document.execCommand("copy");
+
+            var tooltip = bootstrap.Tooltip.getInstance(button);
+            tooltip.dispose();
+            button.setAttribute("data-bs-original-title", "Copied!");
+            bootstrap.Tooltip.getOrCreateInstance(button).show();
+        }
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    </script>
 @endsection
